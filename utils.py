7ba16facbff8d1c3266fd78a994e64835ef12001
menzura1 <chainschains82@gmail.com>
2025-09-02 15:41:18 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,12 +1,10 @@
 import os
 
-
 def check_files_existence(files):
     for file in files:
         if not os.path.exists(file):
             print(f"Ошибка: Файл {file} не найден.")
             exit(1)
-
 
 def check_file_exists(file):
     if not os.path.exists(file):
