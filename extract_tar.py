8def715824620bf2af08c7e5d3ab56c848ecad7d
menzura1 <chainschains82@gmail.com>
2025-09-02 15:39:58 +0700

    Update extract_tar.py

--- extract_tar.py
+++ extract_tar.py
@@ -2,20 +2,20 @@ import os
 import tarfile
 from utils import check_file_exists
 
-# Список извлечённых файлов в рамках текущего сеанса
+#список извлечённых файлов
 extracted_files = set()
 
 
-def extract_tar(archive_name, files=None):
-    global extracted_files  # Используем глобальную переменную для хранения извлечённых файлов
+def extract_tar(archive_name,files=None):
+    global extracted_files  # используем переменную для хранения извлечённых файлов
 
-    # Проверяем существование архива
+    # проверка сущ архива
     check_file_exists(archive_name)
 
-    with tarfile.open(archive_name, "r") as tar:
-        archive_files = tar.getnames()  # Получаем список всех файлов в архиве
+    with tarfile.open(archive_name,"r") as tar:
+        archive_files = tar.getnames()  # список всех файлов
 
-        if files is None or not files:  # Если не указаны файлы или список пуст, извлекаем все
+        if files is None or not files:  # если архив пустой извлекаем все
             tar.extractall()
             print(f"Все файлы из архива {archive_name} извлечены.")
         else:
@@ -23,8 +23,19 @@ def extract_tar(archive_name, files=None):
                 if file in archive_files:
                     if file not in extracted_files:
                         tar.extract(file)
-                        extracted_files.add(file)  # Добавляем файл в множество извлечённых
+                        extracted_files.add(file)  # добавление файла в уже извлеч файлы
                         print(f"Файл {file} извлечен.")
+
+                        # проверка на директорию
+                        if os.path.isdir(file):
+                            # если это папка извлекаем все её содержимое
+                            print(f"Папка {file} извлечена с содержимым.")
+                            for member in tar.getmembers():
+                                if member.name.startswith(file):  # все файлы внутри папки
+                                    if member.name not in extracted_files:
+                                        tar.extract(member)
+                                        extracted_files.add(member.name)
+                                        print(f"Файл {member.name} извлечен из папки {file}.")
                     else:
                         print(f"Файл {file} уже извлечен.")
                 else:
