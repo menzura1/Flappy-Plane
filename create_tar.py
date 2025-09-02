abe93a43f59726334723b87ca1f2db5bf77abab9
menzura1 <chainschains82@gmail.com>
2025-09-02 15:41:02 +0700

    Update create_tar.py

--- create_tar.py
+++ create_tar.py
@@ -4,10 +4,11 @@ from utils import check_files_existence
 
 
 def create_tar(archive_name, files):
-    # Проверяем существование файлов
+    # проверка существования файлов
     check_files_existence(files)
 
-    with tarfile.open(archive_name, "w") as tar:
+    # создание архива
+    with tarfile.open(archive_name, 'w') as tar:
         for file in files:
-            tar.add(file, arcname=os.path.basename(file))  # Сохраняем только имя файла
-    print(f"Архив {archive_name} успешно создан.")
+            tar.add(file, arcname=os.path.basename(file))  # добавляем файл в архив
+    print(f'Архив {archive_name} успешно создан')
